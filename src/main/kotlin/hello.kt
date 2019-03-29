853447716b29d33da58fe772b4fcae1bd012a8b8
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2019-03-29 18:28:51 +0700

    Добавил пример пагинации на оффсете

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -1,9 +1,6 @@
 import org.postgresql.Driver
-import java.sql.DriverManager
 import org.flywaydb.core.Flyway
-import java.sql.SQLType
-import java.sql.Statement
-import java.sql.Types
+import java.sql.*
 
 fun main(args: Array<String>) {
     DriverManager.registerDriver(Driver())
@@ -25,55 +22,27 @@ fun main(args: Array<String>) {
     flyway.migrate()
 
     connection.use { conn ->
-        val stmt = conn.prepareStatement("SELECT count(*) cnt FROM test_table WHERE test_id < ?")
-        val id = 10
-        stmt.setInt(1, id)
-        val resultSet = stmt.executeQuery()
-
-        val hasNext = resultSet.next()
-        if (hasNext) {
-            val cnt = resultSet.getInt("cnt")
-            println("Count: $cnt")
-        }
-
-        val calStmt = conn.prepareCall("{ ? = call increment(?) } ")
-        calStmt.setInt(2, 2)
-        calStmt.registerOutParameter(1, Types.INTEGER)
-        calStmt.execute()
-        println(calStmt.getInt(1))
-
-        val insStmt = conn.prepareStatement(
-            "INSERT INTO test_table (data) VALUES ('Code')",
-            Statement.RETURN_GENERATED_KEYS
-        )
-        insStmt.executeUpdate()
-        val generatedKeys = insStmt.generatedKeys
-        generatedKeys.next()
-        println(generatedKeys.getInt(1))
-
-        val updStmt = conn.prepareStatement("UPDATE test_table SET test_id = test_id + 0")
-        val updated = updStmt.executeUpdate()
-
-        println(updated)
-
-        conn.autoCommit = false
-
-        val deleted = conn.prepareStatement("DELETE FROM test_table where test_id < 10").executeUpdate()
-        val sp = conn.setSavepoint("delete")
-        conn.prepareStatement("DELETE FROM test_table where test_id < 20").executeUpdate()
-        println("Deleted: $deleted")
-        val rsBefore = conn.prepareStatement("SELECT count(*) FROM test_table").executeQuery()
-        rsBefore.next()
-        println("Count before rollback: ${rsBefore.getInt(1)}")
-
-        conn.rollback(sp)
-        val rsAfter = conn.prepareStatement("SELECT count(*) FROM test_table").executeQuery()
-        rsAfter.next()
-        println("Count after rollback: ${rsAfter.getInt(1)}")
+        println(selectPage(conn, Page(10, 3)))
     }
+
 }
 
+fun selectPage(con: Connection, page: Page): List<Int> {
+    val query = "SELECT test_id FROM test_table ORDER BY test_id LIMIT ? OFFSET ?"
+    val limit = page.size
+    val offset = page.size * page.num
+    val stmt = con.prepareStatement(query)
+    stmt.setInt(1, limit)
+    stmt.setInt(2, offset)
+    val rs = stmt.executeQuery()
+    val res = ArrayList<Int>()
+    while (rs.next()) {
+        res.add(rs.getInt(1))
+    }
+    return res
+}
 
+data class Page(val num: Int, val size: Int)
 
 
 
