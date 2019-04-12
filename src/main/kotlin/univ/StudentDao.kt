87d32e6ccfd2534aec77c2176f4cae3a574a597e
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2019-04-12 19:20:28 +0700

    Доделал перевод студента

--- src/main/kotlin/univ/StudentDao.kt
+++ src/main/kotlin/univ/StudentDao.kt
@@ -23,7 +23,7 @@ class StudentDao(private val dataSource: DataSource) {
     fun findStudent(id: Long): Student? {
         val stmt = dataSource.connection.prepareStatement(
             "SELECT " +
-                    "s.id, s.full_name, s.student_group, g.id, g.number, g.students_count " +
+                    "s.id sid, s.full_name, s.student_group, g.id gid, g.number, g.students_count " +
                     "FROM students AS s " +
                     "JOIN groups AS g ON s.student_group = g.id " +
                     "WHERE s.id = ?"
@@ -32,8 +32,8 @@ class StudentDao(private val dataSource: DataSource) {
         val rs = stmt.executeQuery()
         return if (rs.next()) {
             Student(
-                rs.getLong("s.id"), rs.getString("s.full_name"),
-                Group(rs.getLong("g.id"), rs.getString("g.number"), rs.getInt("g.students_count"))
+                rs.getLong("sid"), rs.getString("full_name"),
+                Group(rs.getLong("gid"), rs.getString("number"), rs.getInt("students_count"))
             )
         } else {
             null
@@ -41,7 +41,7 @@ class StudentDao(private val dataSource: DataSource) {
     }
 
     fun updateStudent(student: Student) {
-        val stmt = dataSource.connection.prepareStatement("UPDATE student SET full_name = ?, student_group = ? WHERE id = ?")
+        val stmt = dataSource.connection.prepareStatement("UPDATE students SET full_name = ?, student_group = ? WHERE id = ?")
         stmt.setString(1, student.name)
         stmt.setLong(2, student.group.id!!)
         stmt.setLong(3, student.id!!)
